b683a2acec3cbdcf4a6de53acc115d6160fa80c0
kenobi122 <kenodi97@gmail.com>
2021-06-14 16:57:11 +0700

    inter

--- src/main/java/com/example/room/security/SecurityConfigurer.java
+++ src/main/java/com/example/room/security/SecurityConfigurer.java
@@ -3,7 +3,6 @@ package com.example.room.security;
 import com.example.room.exceptions.JwtAuthenticationEntryPoint;
 import com.example.room.jwt.JwtRequestFilter;
 import com.example.room.services.MyUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,8 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
