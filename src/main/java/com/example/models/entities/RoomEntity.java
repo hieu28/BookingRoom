51ae63d2ca5ecd1531b167843b5e02d0a39d8ff9
hieuap28 <hieuap28@gmail.com>
2021-06-15 14:36:02 +0700

    Cap nhat

--- src/main/java/com/example/models/entities/RoomEntity.java
+++ src/main/java/com/example/models/entities/RoomEntity.java
@@ -1,5 +1,8 @@
 package com.example.models.entities;
 
+import com.sun.istack.NotNull;
+import org.springframework.lang.NonNull;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,21 +13,27 @@ public class RoomEntity {
     private Long id;
 
     @Column(name = "name")
+    @NotNull
     private String name;
 
     @Column(name = "location_id")
+    @NotNull
     private int locationId;
 
     @Column(name = "capacity")
+    @NotNull
     private int capacity;
 
     @Column(name = "description", columnDefinition = "TEXT")
+    @NotNull
     private String description;
 
     @Column(name = "image", columnDefinition = "TEXT")
+    @NotNull
     private String image;
 
     @Column(name = "status", columnDefinition = "BIT")
+    @NotNull
     private boolean status;
 
     public Long getId() {
