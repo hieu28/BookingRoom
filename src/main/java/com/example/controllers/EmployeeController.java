5fe5606336a9d7231966e0d77b3662b4d39dae68
Quyet <vanquyetx97@gmail.com>
2021-06-16 15:01:39 +0700

    sua loi

--- src/main/java/com/example/controllers/EmployeeController.java
+++ src/main/java/com/example/controllers/EmployeeController.java
@@ -3,14 +3,13 @@ package com.example.controllers;
 import com.example.models.entities.EmployeeEntity;
 import com.example.models.requests.EmployeeRequest;
 import com.example.models.responses.EmployeeResponse;
-import com.example.repositories.EmployeeRepository;
 import com.example.services.IEmployeeService;
-import com.example.services.impl.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.relation.RelationNotFoundException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping
@@ -19,18 +18,15 @@ public class EmployeeController {
     @Autowired
     IEmployeeService employeeService;
 
-//    @Autowired
-//    EmployeeRepository employeeRepository;
+    @GetMapping("/employee")
+    public ResponseEntity<List<EmployeeResponse>> getAllEmployee() {
+        return ResponseEntity.ok().body(employeeService.findAll());
+    }
 
-//    @GetMapping("/employee")
-//    public List<EmployeeEntity> getAllEmployee() {
-//        return this.employeeRepository.findAll();
-//    }
-//
-//    @GetMapping("/employee/{id}")
-//    public Optional<EmployeeEntity> getById(@PathVariable("id") long id){
-//        return employeeRepository.findById(id);
-//    }
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<EmployeeEntity> getById(@PathVariable("id") long id) throws RelationNotFoundException {
+        return ResponseEntity.ok().body(employeeService.findById(id));
+    }
 
     @PostMapping("/employee")
     public EmployeeResponse createEmployee(@RequestBody EmployeeRequest entity){
